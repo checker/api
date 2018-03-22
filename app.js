1560c54bdaaf62f25c8be85a7c036ce3ab1beff5
Alex Crocker <croc122@gmail.com>
2018-03-21 21:50:18 -0400

    Removed everything but api stuff

--- app.js
+++ app.js
@@ -1,55 +1,62 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const app = express();
 
-var index = require('./routes/index');
-var services = require('./routes/services');
-var api = require('./routes/api');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const apicache = require('apicache');
+const redis = require('redis');
 
-var app = express();
+const CheckTwitter = require('./libs/Twitter');
+const CheckInstagram = require('./libs/Instagram');
+const CheckSteam = require('./libs/Steam');
+const CheckMinecraft = require('./libs/Minecraft');
+const CheckYoutube = require('./libs/Youtube');
+const CheckMixer = require('./libs/Mixer');
+const CheckTwitch = require('./libs/Twitch');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/services', services);
-app.use('/api', api);
+let cacheWithRedis = apicache.options({ redisClient: redis.createClient() }).middleware;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+/* GET root api endpoint */
+app.get('/', function(req, res, next) {
+  res.send('this is the root api endpoint');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/check/:service/:word', cacheWithRedis('6 hours'), function(req, res, next) {
+   var service = req.params.service;
+   var word = req.params.word;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+   switch (service) {
+      case "twitter":
+         CheckTwitter(service, word, res);
+         break;
+      case "instagram":
+         CheckInstagram(service, word, res);
+         break;
+      case "steamid":
+         CheckSteam(service, word, res);
+         break;
+      case "steamgroup":
+         CheckSteam(service, word, res);
+         break;
+      case "minecraft":
+         CheckMinecraft(service, word, res);
+         // half working, may have to change to 3rd party api
+         break;
+      case "youtube":
+         CheckYoutube(service, word, res);
+         break;
+      case "mixer":
+         CheckMixer(service, word, res);
+         break;
+      case "twitch":
+         CheckTwitch(service, word, res);
+         break;
+   }
 });
+
+app.listen(8080, 'localhost');
 
 module.exports = app;
