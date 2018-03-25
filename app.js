1d91f8f9aa67188e9ddd6bd433c4abab0783dc9b
Alex Crocker <croc122@gmail.com>
2018-03-24 21:13:28 -0400

    Removed Twitch, MC and added basic api key support

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ const timeout = require('connect-timeout');
 const axios = require('axios');
 const apicache = require('apicache');
 const redis = require('redis');
+const fs = require('fs');
 
 const CheckTwitter = require('./libs/Twitter');
 const CheckInstagram = require('./libs/Instagram');
 const CheckSteam = require('./libs/Steam');
-const CheckMinecraft = require('./libs/Minecraft');
 const CheckYoutube = require('./libs/Youtube');
 const CheckMixer = require('./libs/Mixer');
-const CheckTwitch = require('./libs/Twitch');
 
 app.use(router);
 app.use(timeout('5s'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(haltOnTimedout);
+
+function checkAuthKey(req, res, next) {
+  var keys = require('./apikeys.json');
+  var appkey = req.get('og-apikey');
+
+  if (keys.authorized.includes(appkey)) {
+    next()
+  } else {
+    res.json({"status": "unauthorized"});
+  }
+}
 
 let cacheWithRedis = apicache.options({ redisClient: redis.createClient() }).middleware;
 
@@ -29,7 +39,7 @@ router.get('/', function(req, res, next) {
   res.send('this is the root api endpoint');
 });
 
-router.get('/check/:service/:word', cacheWithRedis('6 hours'), function(req, res, next) {
+router.get('/check/:service/:word', [checkAuthKey, cacheWithRedis('6 hours')], function(req, res, next) {
    var service = req.params.service;
    var word = req.params.word;
 
@@ -46,18 +56,11 @@ router.get('/check/:service/:word', cacheWithRedis('6 hours'), function(req, res
       case "steamgroup":
          CheckSteam(service, word, res);
          break;
-      case "minecraft":
-         CheckMinecraft(service, word, res);
-         // half working, may have to change to 3rd party api
-         break;
       case "youtube":
          CheckYoutube(service, word, res);
          break;
       case "mixer":
          CheckMixer(service, word, res);
-         break;
-      case "twitch":
-         CheckTwitch(service, word, res);
          break;
    }
 });
